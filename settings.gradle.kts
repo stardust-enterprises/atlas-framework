240d506b3bf12053b5e89b332e0c607c93badfa7
xtrm <xtram.dev@gmail.com>
2022-03-17 08:47:53 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "kotlin-template"
+
+pluginManagement.repositories {
+    mavenLocal()
+    gradlePluginPortal()
+}
---
+++ src/main/kotlin/Test.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val projectName = "@project.name@"
+    val projectVersion = "@project.version@"
+    println("Project: $projectName v$projectVersion")
+    println("Description: @project.desc@")
+}
